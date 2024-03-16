44d589c8f6aa9d773c40f8cd59c7a04ea54047d1
Matheus Gabriel Dalla Valle <126087969+DallaVal@users.noreply.github.com>
2024-03-15 22:11:01 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/3_booleam/script.js
@@ -0,0 +1,20 @@
+const text1 = "ola mundo"
+const text2 = 'ola mundo'
+const senha = "senhaseupersegura"
+const StringDeNumeros = "3642"
+
+//concatenação (+)
+
+const citacao = "Meu nome é"
+const MeuNome = "Matheus"
+
+console.log(`${citacao} ${MeuNome}`)
+
+//comparação
+
+console.log(text1 == text2)
+
+const n1 = 1
+const n2 = 2
+
+console.log(n1 === n2)
